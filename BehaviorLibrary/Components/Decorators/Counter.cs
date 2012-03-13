f3148e883d18977cc6696dede38e9b5f2d283304
Thomas Jonell <tjonell@woh.rr.com>
2012-03-12 20:53:52 -0400

    added Counter and Timer decorators

---
+++ BehaviorLibrary/Components/Decorators/Counter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BehaviorLibrary.Components.Decorators
+{
+    class Counter : BehaviorComponent
+    {
+        private int c_MaxCount;
+        private int c_Counter = 0;
+
+        private BehaviorComponent c_Behavior;
+
+        /// <summary>
+        /// executes the behavior based on a counter
+        /// -each time Counter is called the counter increments by 1
+        /// -Counter executes the behavior when it reaches the supplied maxCount
+        /// </summary>
+        /// <param name="maxCount">max number to count to</param>
+        /// <param name="behavior">behavior to run</param>
+        public Counter(int maxCount, BehaviorComponent behavior)
+        {
+            c_MaxCount = maxCount;
+            c_Behavior = behavior;
+        }
+
+        /// <summary>
+        /// performs the given behavior
+        /// </summary>
+        /// <returns>the behaviors return code</returns>
+        public override BehaviorReturnCode Behave()
+        {
+            try
+            {
+                if (c_Counter < c_MaxCount)
+                {
+                    c_Counter++;
+                    ReturnCode = BehaviorReturnCode.Running;
+                    return BehaviorReturnCode.Running;
+                }
+                else
+                {
+                    c_Counter = 0;
+                    ReturnCode = c_Behavior.Behave();
+                    return ReturnCode;
+                }
+            }
+            catch (Exception)
+            {
+                ReturnCode = BehaviorReturnCode.Failure;
+                return BehaviorReturnCode.Failure;
+            }
+        }
+    }
+}
---
+++ BehaviorLibrary/Components/Decorators/Timer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BehaviorLibrary.Components.Decorators
+{
+    class Timer : BehaviorComponent
+    {
+
+        private Func<int> t_ElapsedTimeFunction;
+
+        private BehaviorComponent t_Behavior;
+
+        private int t_TimeElapsed;
+
+        private int t_WaitTime;
+
+        /// <summary>
+        /// executes the behavior after a given amount of time in miliseconds has passed
+        /// </summary>
+        /// <param name="elapsedTimeFunction">function that returns elapsed time</param>
+        /// <param name="timeToWait">maximum time to wait before executing behavior</param>
+        /// <param name="behavior">behavior to run</param>
+        public Timer(Func<int> elapsedTimeFunction, int timeToWait, BehaviorComponent behavior)
+        {
+            t_ElapsedTimeFunction = elapsedTimeFunction;
+            t_Behavior = behavior;
+            t_WaitTime = timeToWait;
+        }
+
+        /// <summary>
+        /// performs the given behavior
+        /// </summary>
+        /// <returns>the behaviors return code</returns>
+        public override BehaviorReturnCode Behave()
+        {
+            try
+            {
+                t_TimeElapsed += t_ElapsedTimeFunction.Invoke();
+
+                if (t_TimeElapsed > t_WaitTime)
+                {
+                    ReturnCode = t_Behavior.Behave();
+                    return ReturnCode;
+                }
+                else
+                {
+                    ReturnCode = BehaviorReturnCode.Running;
+                    return BehaviorReturnCode.Running;
+                }
+            }
+            catch (Exception)
+            {
+                ReturnCode = BehaviorReturnCode.Failure;
+                return BehaviorReturnCode.Failure;
+            }
+        }
+    }
+}
